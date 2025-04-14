f8a0a5e0afc614d6155b71280ede3d2d3285a4a9
Raylan Barboza <raylanbarbozapessoal@gmail.com>
2025-04-14 14:54:52 -0400

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -11,6 +11,12 @@ const ranking = JSON.parse(localStorage.getItem('ranking')) || [];
             }
         });
 
+function limparDados() {
+    localStorage.removeItem('ranking'); // Remove do localStorage
+    ranking.length = 0; // Limpa o array
+    atualizarRanking(); // Atualiza a exibição
+    alert('Todos os dados do ranking foram removidos!');
+}
         if (ranking.length === 0) {
             rankingLista.innerHTML = `<li>Nenhum nome adicionado :(</li>`;
         }
