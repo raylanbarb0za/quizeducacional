01bbc3708dcc51a61319b4748cc5a6dc27634e29
Raylan Barboza <raylanbarbozapessoal@gmail.com>
2025-04-14 15:07:55 -0400

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,4 +1,3 @@
-// Função para atualizar a lista na tela
 function atualizarLista() {
   const ranking = JSON.parse(localStorage.getItem('ranking')) || [];
   const rankingLista = document.getElementById('ranking-lista');
@@ -10,14 +9,10 @@ function atualizarLista() {
     : '<li>Nenhum nome adicionado :(</li>';
 }
 
-// Função para limpar os dados (chamada pelo onclick)
 function limparDados() {
-  if (confirm('Tem certeza que deseja apagar TODOS os dados do ranking?')) {
     localStorage.removeItem('ranking');
     atualizarLista();
     alert('Dados apagados com sucesso!');
   }
-}
 
-// Carrega a lista quando a página abre
 document.addEventListener('DOMContentLoaded', atualizarLista);
