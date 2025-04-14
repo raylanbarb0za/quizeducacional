599ed15c4db9ac474ffc5de37761f424fdd7c2da
Raylan Barboza <raylanbarbozapessoal@gmail.com>
2025-04-14 15:03:49 -0400

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,14 +1,18 @@
 const rankingLista = document.getElementById('ranking-lista');
 const ranking = JSON.parse(localStorage.getItem('ranking')) || [];
 
-rankingLista.innerHTML = ranking.length
-  ? ranking.sort((a,b) => b.estrelinhas - a.estrelinhas)
-      .map(item => `<li>${item.nome}: ${item.estrelinhas} ⭐ (${item.disciplina})</li>`)
-      .join('')
-  : '<li>Nenhum nome adicionado :(</li>';
+function carregarRanking() {
+  rankingLista.innerHTML = ranking.length
+    ? ranking.sort((a,b) => b.estrelinhas - a.estrelinhas)
+        .map(item => `<li>${item.nome}: ${item.estrelinhas} ⭐ (${item.disciplina})</li>`)
+        .join('')
+    : '<li>Nenhum nome adicionado :(</li>';
+}
 
 function limparDados() {
   localStorage.removeItem('ranking');
-  rankingLista.innerHTML = '<li>Nenhum nome adicionado :(</li>';
+  carregarRanking();
   alert('Ranking limpo com sucesso!');
 }
+
+carregarRanking();
